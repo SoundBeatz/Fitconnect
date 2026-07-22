17cd741059549d34fbbe29dd7ad3ebc437e062de
SoundBeatz <info@u360.nl>
2026-07-22 13:11:44 +0200

    fix(commerce): allow FitConnect application CORS header

--- supabase/functions/commerce-update-order/index.ts
+++ supabase/functions/commerce-update-order/index.ts
@@ -3,7 +3,7 @@ import { sendShippedOrderEmail } from "../_shared/order-email.ts";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, x-application-name, apikey, content-type",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 const json = (body: unknown, status = 200) => new Response(JSON.stringify(body), {
