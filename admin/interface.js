8bb4ce06ef72f3e65fe3820a0f934c706d6f3e8e
SoundBeatz <info@u360.nl>
2026-07-26 13:46:37 +0200

    Load Combination Deals module registry integration

--- admin/interface.js
+++ admin/interface.js
@@ -36,6 +36,7 @@
     loadScript('bundle-dealstudio-intelligence.js?v=20260725-1','fitconnect-dealstudio-intelligence');
     loadStyle('dealstudio-complete.css?v=20260725-1','fitconnect-dealstudio-complete-css');
     loadScript('dealstudio-complete.js?v=20260725-1','fitconnect-dealstudio-complete');
+    loadScript('combination-deals-module-registry.js?v=20260726-1','fitconnect-combination-module-registry');
   }
   function apply(){
     document.documentElement.style.setProperty('--heading-size',`${settings.headingSize}px`);
