adc97c8e84b525212f491bed54f428efdf7dcf88
SoundBeatz <info@u360.nl>
2026-08-01 22:26:11 +0200

    Load exclusive Module Registry V6

--- admin/interface.js
+++ admin/interface.js
@@ -37,7 +37,7 @@
     loadStyle('dealstudio-complete.css?v=20260725-1','fitconnect-dealstudio-complete-css');
     loadScript('dealstudio-route-isolation.js?v=20260801-1','fitconnect-dealstudio-route-isolation');
     loadScript('dealstudio-complete.js?v=20260801-1','fitconnect-dealstudio-complete');
-    loadScript('module-registry-v2.js?v=20260801-1','fitconnect-module-registry-v2');
+    loadScript('module-registry-v6.js?v=20260801-1','fitconnect-module-registry-v6');
     loadScript('combination-deals-runtime-loader.js?v=20260726-1','fitconnect-combination-runtime-loader');
   }
   function apply(){
