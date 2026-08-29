4b2a4919861f6a525ca24719924e9b1c892b7775
SoundBeatz <info@u360.nl>
2026-08-29 05:07:29 +0200

    security: keep My Twin trigger internal

---
+++ supabase/migrations/202608290005_my_twin_identity_trigger_execute_hardening_v1.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.my_twin_identity_profile_touch() from public, anon, authenticated;
+grant execute on function public.my_twin_identity_profile_touch() to service_role;
